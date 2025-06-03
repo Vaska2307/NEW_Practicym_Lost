8de5ad3910d99faedcefe10c09039173868080b5
Vaska_1283 <vasili.gusako@yandex.ru>
2025-06-03 14:57:01 +0300

    DZ1 : Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 4. Напишите программу, которая принимает на вход три числа и выдает максимальное из этих чисел.
+// 2  3  7 -> 7, 45  5  78 -> 78, 22  3  9 -> 22.
+using System;
+Console.Clear();
+Console.WriteLine("Введите первое число a: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число b: ");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите третье число c: ");
+int c = int.Parse(Console.ReadLine());
+if ((a > b) && (a > c))
+{
+    Console.WriteLine($"max = {a} ");
+}
+else
+{
+    if ((b > a) && (b > c))
+    {
+        Console.WriteLine($"max = {b} ");
+    }
+    else
+    {
+        if (c > b)
+        {
+            Console.WriteLine($"max = {c} ");
+        }
+    }
+}
+// Второе решение:
+int max = a;
+if (max < b)
+{
+    max = b;
+}
+if (c > max)     // Это тоже вариант решения.
+{
+    max = c;
+}
+Console.WriteLine(max);
