6d52cd4b5eee55ee4241e6f600c59922bd01c45d
Vaska_1283 <vasili.gusako@yandex.ru>
2025-06-03 13:49:01 +0300

    DZ1 : Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 2. Напишите программу, которая на вход принемает два числа и выдает, какое число большее, а какое меньшее.
+// Например: a = 25, b = 5 -> a больше b, a = 2, b = 10 -> a меньше b, a = 9, b = 9 -> a равно b.
+using System;
+Console.Clear();
+Console.WriteLine("Введите число a: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число b: ");
+int b = int.Parse(Console.ReadLine());
+if (a > b)
+{
+    Console.Write($"{a} больше {b}");
+}
+if (a < b)
+{
+    Console.Write($"{a} меньше {b}");
+}
+if(a == b)
+{
+    Console.Write($"{a} равно {b}");
+}
