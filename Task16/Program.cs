6a8de44ea47ee2cee5161142972e4dbf6ce122d2
Vaska_1283 <vasili.gusako@yandex.ru>
2025-07-20 22:20:26 +0300

    Semenar2 : Task16

---
+++ Task16/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using static System.Console;
+Clear();
+Write("Введите первое число a: ");
+int a = int.Parse(ReadLine());
+Write("Введите второе число b: ");
+int b = int.Parse(ReadLine());
+
+if (a * a == b)
+{
+    Write($"{b} квадрат {a}");
+}
+else
+{
+    if (b * b == a)
+    {
+        Write($"{a} квадрат {b}");
+    }
+    else
+    {
+        WriteLine("Не является");
+    } 
+}
