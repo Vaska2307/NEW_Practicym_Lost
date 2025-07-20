235eebee2adc68548d120ea6eff2e0bfaffa895c
Vaska_1283 <vasili.gusako@yandex.ru>
2025-07-20 21:19:44 +0300

    Semenar2 : Task 12

---
+++ Task12/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using static System.Console;
+Clear();
+WriteLine("Введите первое число:");
+int a = int.Parse(ReadLine());
+WriteLine("Введите второе число: ");
+int b = int.Parse(ReadLine());
+if (a % b == 0)
+{
+    WriteLine($"{a} кратно {b}");
+}
+else
+{
+    WriteLine($"{a} не кратно {b}, остаток {a % b}");
+}
