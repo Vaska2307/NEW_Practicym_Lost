e8dad7d3fce3a25248cd134b95210d6279ba57ef
Vaska_1283 <vasili.gusako@yandex.ru>
2025-07-20 21:39:05 +0300

    Semenar2 : Task14

---
+++ Task14/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using static System.Console;
+Clear();
+Write("Введите число: ");
+int number = int.Parse(ReadLine());
+int a = 7;
+int b = 23;
+if ((number % a == 0) && (number % 23 == 0))
+{
+    Write($"{number} -> да");
+}
+else
+{
+    Write($"{number} -> нет");
+}
