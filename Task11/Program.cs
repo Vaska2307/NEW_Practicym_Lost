3f95dc2582f42764e443bf7f1ac25c1aed7a1d9f
Vaska_1283 <vasili.gusako@yandex.ru>
2025-07-20 21:00:56 +0300

    Semenar2 : Task11

---
+++ Task11/Program.cs
@@ -0,0 +1,7 @@
+﻿using System;
+using static System.Console;
+Clear();
+int number = new Random().Next(100, 1000);
+int a = (number / 100) * 10;
+int b = number % 10;
+Write($"{number} -> {a + b} ");
