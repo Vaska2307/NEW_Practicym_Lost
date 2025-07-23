e3c303766bd7a024030a1ab4d4288821a6515a3e
Vaska_1283 <vasili.gusako@yandex.ru>
2025-07-23 12:38:31 +0300

    Seminar3 : Task 17

---
+++ Task17/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using static System.Console;
+Clear();
+Write("Введите координату X: ");
+int X = int.Parse(ReadLine());
+Write("Введите координату Y: ");
+int Y = int.Parse(ReadLine());
+if (X > 0 && Y > 0)
+{
+    WriteLine($"X -> {X}, Y -> {Y} => 1 Четверть! ");
+}
+if (X < 0 && Y > 0)
+{
+    WriteLine($"X -> {X}, Y -> {Y} => 2 Четверть! ");
+}
+if (X < 0 && Y < 0)
+{
+    WriteLine($"X -> {X}, Y -> {Y} => 3 Четверть! ");
+}
+if (X > 0 && Y < 0)
+{
+    WriteLine($"X -> {X}, Y -> {Y} => 4 Четверть! ");
+}
+
