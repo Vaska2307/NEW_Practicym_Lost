c497b94b8e5469012d5633a5e86b32d2ba4c691b
Vaska_1283 <vasili.gusako@yandex.ru>
2025-07-23 11:17:45 +0300

    DZ : Task13

---
+++ Task13/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using static System.Console;
+Clear();
+Write("Введите число n1: ");
+int n1 = int.Parse(ReadLine());
+if (n1 < 100 && n1 > -100)
+{
+    WriteLine($"{n1} -> нет третьей цифры ");
+    return;
+}
+else
+{
+    int number1 = (n1 % 1000) / 100;
+    WriteLine($"{n1} -> {number1}");
+}
+
+//int number1 = (n1 / 100) % 10;
+//WriteLine($"{n1} -> {number1}");
