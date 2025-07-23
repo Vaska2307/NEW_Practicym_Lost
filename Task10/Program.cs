1d93fe247cbebed579fd9237a3bb9fc20caae175
Vaska_1283 <vasili.gusako@yandex.ru>
2025-07-23 10:39:58 +0300

    DZ : Task10

---
+++ Task10/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+Write("Введите трехзначное число N: ");
+int N = int.Parse(ReadLine());
+int T = new Random().Next(100, 1000);
+int number = (N % 100) / 10;
+int number2 = (T % 100) / 10;
+WriteLine($"{N} -> {number}");
+WriteLine($"{T} -> {number2}");
