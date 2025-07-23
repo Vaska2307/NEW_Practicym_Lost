ffce9a54bb4077f0040ee3e4e4379329772c0bca
Vaska_1283 <vasili.gusako@yandex.ru>
2025-07-23 11:45:34 +0300

    DZ : Task15

---
+++ Task15/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using static System.Console;
+Clear();
+Write("Напишите цифру которая соответствует дню недели: ");
+int N = int.Parse(ReadLine());
+
+if (N < 1 || N > 7)
+{
+    WriteLine($"{N} -> Неверно введени номер");
+    return;
+}
+if (N < 6)  //if (N > 0 && N < 6)
+{
+    WriteLine($"{N} -> Нет");
+    return;
+}
+else
+{
+    WriteLine($"{N} -> Да");
+}
+
