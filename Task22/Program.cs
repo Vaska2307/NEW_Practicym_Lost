8000da4f4a84dad146f30e906af310399921a53e
Vaska_1283 <vasili.gusako@yandex.ru>
2025-07-25 21:34:34 +0300

    Seminar3 : Task22

---
+++ Task22/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using static System.Console;
+Clear();
+Write("Введите число N: ");
+int N = int.Parse(ReadLine());
+int i = 1;
+//double sqr = 0;
+while (i <= N)
+{
+    //sqr = Math.Pow(i, 2);
+    Write($"{Math.Pow(i, 2)}, ");
+    //WriteLine($"{i * i}");
+    i = i + 1;
+}
