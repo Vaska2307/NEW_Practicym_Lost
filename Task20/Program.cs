5a96faf1361834fa95f946ffa617498b4bf3e0f4
Vaska_1283 <vasili.gusako@yandex.ru>
2025-07-25 20:53:14 +0300

    Seminar3 : Task20

---
+++ Task20/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using static System.Console;
+Clear();
+Write("Введите координату Х1 : ");
+int X1 = int.Parse(ReadLine());
+Write("Введите координату Y1 : ");
+int Y1 = int.Parse(ReadLine());
+Write("Введите координату Х2 : ");
+int X2 = int.Parse(ReadLine());
+Write("Введите координату Y2 : ");
+int Y2 = int.Parse(ReadLine());
+double d = Math.Sqrt((Math.Pow(X1 - X2, 2) + Math.Pow(Y1 - Y2, 2)));
+WriteLine($"Длинна отрезка A({X1},{Y1}) ; B({X2},{Y2}) -> {d:f3} ");
+
