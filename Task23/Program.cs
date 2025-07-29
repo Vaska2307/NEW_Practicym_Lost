b08c0503cd2368ab219a9f04d58b8b3ee0a36fd0
Vaska_1283 <vasili.gusako@yandex.ru>
2025-07-29 12:28:37 +0300

    Seminsr3 : Task23

---
+++ Task23/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using static System.Console;
+Clear();
+Write("Введите число N: ");
+int N = int.Parse(ReadLine());
+//int i = 1;
+//double d = 0;
+Write($"{N} -> ");
+for (int i = 1; i <= N; i++)
+{
+    //d = Math.Pow(i, 3);
+    Write($"{Math.Pow(i, 3)}, ");
+    //i = i + 1;
+}
