180ba4ec0f151361ce766972652bfbe19346c657
Vaska_1283 <vasili.gusako@yandex.ru>
2025-08-03 17:02:19 +0300

    Seminar4: Task26

---
+++ Task26/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using static System.Console;
+Clear();
+
+Write("Введитк число N: ");
+//int N = int.Parse(ReadLine());
+//int result = GetNumbers(N);
+WriteLine($"Колличество цифр в числе = {GetNumbers(int.Parse(ReadLine()))}");// сократили колличество переменных и строк и памяти занимаемой кодом
+
+int GetNumbers(int number)
+{
+    int count = 0;
+    while (number > 0)
+    {
+
+        number = number / 10;
+        count++;
+    }
+    return count;
+}
+
