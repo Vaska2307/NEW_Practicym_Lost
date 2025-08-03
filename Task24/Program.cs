78fcdaa84d6f81667d815d971f3abf52aaa05d38
Vaska_1283 <vasili.gusako@yandex.ru>
2025-08-03 16:36:44 +0300

    Seminsr4 : Task24

---
+++ Task24/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+WriteLine("Введите число N: ");
+int N = int.Parse(ReadLine());
+int summa = GetSumNumber(N);
+WriteLine(summa);
+
+int GetSumNumber(int number)
+{
+    int sum = 0;
+    for (int i = 0; i <= number; i++)
+    {
+        sum = sum + i;
+    }
+    return sum;
+}
