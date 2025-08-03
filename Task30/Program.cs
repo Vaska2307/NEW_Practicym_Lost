f4f6c3492334d3605a075619444e933ff66b2516
Vaska_1283 <vasili.gusako@yandex.ru>
2025-08-03 17:42:56 +0300

    Seminar4 : Task30

---
+++ Task30/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using static System.Console;
+Clear();
+WriteLine($"[{String.Join(", ", GetBinaryArray(8))}] ");
+
+int[] GetBinaryArray(int size)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(0, 2);
+    }
+    return result;
+}
