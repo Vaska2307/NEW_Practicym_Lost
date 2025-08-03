b37f922967dab7d784c117e57d66cf695ffa17a5
Vaska_1283 <vasili.gusako@yandex.ru>
2025-08-03 17:23:19 +0300

    Seminar4 : Task28

---
+++ Task28/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using static System.Console;
+Clear();
+
+Write("Введите число N: ");
+WriteLine($"Произведение от 1 до N -> {GetCompositionNumber(int.Parse(ReadLine()))} ");
+ 
+int GetCompositionNumber(int number)
+{
+    int composition = 1;
+    for (int i = 2; i <= number; i++)
+    {
+        composition *= i;
+    }
+    return composition;
+}
